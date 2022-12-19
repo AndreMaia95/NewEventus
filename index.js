84e56a680a52797be184e0ed6967923b1bac9d0f
AndreMaia95 <andrefsm95@gmail.com>
2022-12-19 16:01:02 +0000

    feat: javascript for sidebar

---
+++ index.js
@@ -0,0 +1,22 @@
+const menuItems = document.querySelectorA11(".menu-item");
+// remove active class from all menu items
+const changeActiveItem = () => {
+  menuItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    changeActiveItem();
+    item.classList.add("active");
+    if (item.id != "notifications") {
+      document.querySelector(".notifications-popup").style.display = "none";
+    } else {
+      document.querySelector(".notifications-popup").style.display = "block";
+      document.querySelector(
+        "#notifications .notification-count"
+      ).style.display = "none";
+    }
+  });
+});
